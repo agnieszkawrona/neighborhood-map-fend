97b0fcd7dd7f64f3c9392108eba06861a4be0a0f
agnieszkaw61 <agnieszkaw61@gmail.com>
2018-08-23 20:46:50 +0200

    feat: map, markers, listview, search box, filtering

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# neighborhood-map-fend
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+
+# Neighborhood Map
+FEND final project!
+
+Neighborhood Map is an app that lets you find the best cafes in Wroclaw, Poland.
+Here you can see their location on the map, search for particular ones and check their exact adress!
+
+## Attribution
+####Google Maps API:
+You're supposed to add your own Google Maps API key here (MapContainer.js, line 35):
+`componentDidMount() {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=YOURAPIKEY&v=3&callback=initMap")
+    window.initMap = this.initMap
+  }`
+
+## Dependencies
+axios: https://www.npmjs.com/package/axios
+react: https://www.npmjs.com/package/react
+react-dom: https://www.npmjs.com/package/react-dom
+prop-types: https://www.npmjs.com/package/prop-types
+escape-string-regexp: https://www.npmjs.com/package/escape-string-regexp
---
+++ src/SidePanel.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class SidePanel extends Component {
+
+  // function that activates the location marker, which name is passed
+  activateLocation = (locationName) => {
+    const { markers } = this.props
+    markers.forEach(function (marker) {
+      if (marker.title === locationName) {
+        window.google.maps.event.trigger(marker, 'click')
+      } else {
+        marker.title = ''
+      }
+      //marker.title === locationName ? window.google.maps.event.trigger(marker, 'click') : ''
+    })
+  }
+
+  render() {
+    const { query, searched, filteredPlaces } = this.props
+
+    return (
+      <div className="SidePanel" aria-label="sidebar">
+        <div className="side-title" aria-label="Best Cafes in Wroclaw">Best Cafes in Wroclaw</div>
+        <div className="SearchBox" aria-label="searchbox">
+          <input
+            type="text"
+            placeholder="Search cafes..."
+            value={query}
+            aria-label="Search cafes"
+            role="search"
+            onChange={(event) => searched(event.target.value)}
+          />
+        </div>
+        <div className="ListView" aria-label="list of cafes">
+        <ul>
+          {filteredPlaces.map((location, index) => (
+            <li key={index}><button onClick={(event) => this.activateLocation(location.name)} aria-label="cafe button">{location.name}</button></li>
+          ))}
+        </ul>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default SidePanel
